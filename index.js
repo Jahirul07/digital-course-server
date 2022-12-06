650d032577b53c901c0a4590f983f1dcba7304c2
Jahirul Islam <jahirul@elyranur.com>
2022-12-06 12:06:35 +0600

    courses data api added

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
+const cors = require('cors')
+
+
+app.use(cors());
 
 const categories = require('./data/categories.json')
+const courses = require('./data/courses.json')
 
 app.get('/', (req, res) =>{
     res.send('Api is Running');
@@ -10,6 +15,18 @@ app.get('/', (req, res) =>{
 
 app.get('/course-categories', (req, res) =>{
     res.send(categories);
+})
+
+app.get('/category/:id', (req, res) =>{
+    const id = req.params.id;
+    const totalCategory = course.filter( c => c.category_id === id);
+    req.send(totalCategory);
+})
+
+app.get('/courses/:id', (req, res) =>{
+    const id = req.params.id;
+    const selecredCourse = courses.find( course => course._id === id)
+    res.send(selecredCourse);
 })
 
 app.listen(port, () =>{
